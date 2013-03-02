e9e2948e497f9739e590f11f15a7c23f05515f65
Pierre Dulac <dulacpier@gmail.com>
2013-03-02 18:56:15 +0100

    Improve the setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,59 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
-import provider
+from setuptools import setup
+import re
+import os
+import sys
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('provider')
 
 setup(
     name='django-oauth2-provider',
-    version=provider.__version__,
+    version=version,
     description='Provide OAuth2 access to your app',
-    long_description=open('README.md').read(),
+    long_description="Customizable OAuth 2 authentication for your Django projects.",
     author='Alen Mujezinovic',
     author_email='alen@caffeinehit.com',
-    url = 'https://github.com/dulaccc/django-oauth2-provider',
-    packages= find_packages(exclude=('tests*',)),
+    url='https://github.com/dulaccc/django-oauth2-provider',
+    packages=get_packages('provider'),
+    package_data=get_package_data('provider'),
+    install_requires=[
+        "shortuuid>=0.3"
+    ],
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -19,10 +61,5 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
-    ],
-    install_requires=[
-        "shortuuid>=0.3"
-    ],
-    include_package_data=True,
-    zip_safe=False,
+    ]
 )
